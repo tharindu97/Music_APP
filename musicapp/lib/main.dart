e928bf8781e3a11765c8ea6ff6f13b055fba5092
tharindu97 <tharindukavishna@gmail.com>
2020-05-15 20:59:33 +0530

    completed

--- musicapp/lib/main.dart
+++ musicapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,19 +20,31 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AudioPlayer _audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  String currentTime = '00.00';
+  String completeTime = '00.00';
  @override
   void initState() {
     super.initState();
+    _audioPlayer.onAudioPositionChanged.listen((Duration duration){
+      setState(() {
+        currentTime = duration.toString().split(".")[0];
+      });
+    });
+    _audioPlayer.onDurationChanged.listen((Duration duration){
+      completeTime = duration.toString().split(".")[0];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
@@ -39,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             width: MediaQuery.of(context).size.width*0.8,
             height: 80,
-            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.7, left: MediaQuery.of(context).size.height*0.7),
+            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.7, left: MediaQuery.of(context).size.width*0.2),
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(50)
@@ -49,18 +63,33 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 IconButton(
-                  icon: Icon(Icons.play_arrow), 
+                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow), 
                   onPressed: (){
-                    
+                    if(isPlaying){
+                      _audioPlayer.pause();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    }else{
+                      _audioPlayer.resume();
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
                   }
                 ),
                 SizedBox(width: 16,),
                 IconButton(
                   icon: Icon(Icons.stop), 
                   onPressed: (){
-
+                    setState(() {
+                      isPlaying = false;
+                    });
                   }
-                )
+                ),
+                Text(currentTime, style: TextStyle(fontWeight: FontWeight.w700),),
+                Text(' | '),
+                Text(completeTime, style: TextStyle(fontWeight: FontWeight.w700),),
               ],
             ),
           )
@@ -69,8 +98,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.audiotrack),
-        onPressed: (){
-
+        onPressed: () async{
+          String filePath = await FilePicker.getFilePath();
+          int status = await _audioPlayer.play(filePath, isLocal: true);
+          if(status == 1){
+            setState(() {
+              isPlaying = true;
+            });
+          }
         }
       ),
     );
